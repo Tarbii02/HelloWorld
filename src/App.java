98b566b0101883efc300639fe7da7ac4a064951b
Abishan <you@example.com>
2023-11-07 09:23:04 +0100

    first commit

---
+++ src/App.java
@@ -0,0 +1,19 @@
+import processing.core.*;
+
+public class App extends PApplet{
+    public static void main(String[] args){
+        PApplet.main("App");
+    }
+
+    public void settings(){
+        size(600,400);
+    }
+
+    public void setup(){
+
+    }
+
+    public void draw(){
+        
+    }
+}
